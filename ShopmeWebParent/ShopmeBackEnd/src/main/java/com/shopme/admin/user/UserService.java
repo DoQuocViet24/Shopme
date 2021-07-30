d4bb2b2a28bb5766cc9d9de45187a2b7a354b88f
doquocviet24 <doquocviet0902@gmail.com>
2021-07-30 17:39:03 +0700

    phan trang

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,8 +1,15 @@
 package com.shopme.admin.user;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +17,11 @@ import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Service
+@Transactional
 public class UserService {
+	
+	public static final int USERS_PER_PAGE=4;
+	
     @Autowired
     private UserRepository repo;
     @Autowired
@@ -24,10 +35,25 @@ public class UserService {
     public List<Role> listRoles(){
     	return (List<Role>)repoRole.findAll();
     }
+    public Page<User> listByPage(int pageNum){
+    	Pageable pageable = PageRequest.of(pageNum-1,USERS_PER_PAGE);
+    	return repo.findAll(pageable);
+    }
     
-    public void save(User user) {
-    	encodePassword(user);
-    	repo.save(user);
+    public User save(User user) {
+    	boolean isUpdatingUser = (user.getId() !=null);
+    	if(isUpdatingUser) {
+    		User u = repo.findById(user.getId()).get();
+    		if(user.getPassword().isEmpty()) {
+    			user.setPassword(u.getPassword());
+    		}
+    		else {
+    			encodePassword(user);
+    		}
+    	}else {
+    		encodePassword(user);
+    	}
+    	return repo.save(user);
     }
     
     private void encodePassword(User user) {
@@ -35,8 +61,38 @@ public class UserService {
     	user.setPassword(encodedPassword);
     }
     
-    public boolean isEmailUnique(String email) {
+    public boolean isEmailUnique(Integer id, String email) {
+    	
     	User userByEmail = repo.getUserByEmail(email);
-    	return userByEmail==null;
+    	if(userByEmail==null) return true;
+    	boolean isCreatingNew = (id==null);
+    	if(isCreatingNew) {
+    		if(userByEmail !=null) return false;
+    	}else {
+    		if(userByEmail.getId()!=id) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    public User get(Integer id) throws UserNotFoundException {
+    	try {
+    		return repo.findById(id).get();
+    	} catch(NoSuchElementException ex) {
+    		throw new UsernameNotFoundException("Could not find any user with ID: "+  id);
+    	}
+    }
+    
+    public void delete(Integer id) throws UserNotFoundException {
+    	Long countById = repo.countById(id);
+    	if(countById == null || countById ==0) {
+    		throw new UsernameNotFoundException("Could not find any user with ID: "+  id);
+    	}
+        repo.deleteById(id);
+    }
+    
+    public void updateUserStatus(Integer id, boolean enabled) {
+    	repo.updateEnabledStatus(id, enabled);
     }
 }
