b31576123151f41dd8340201ebda343c18f9d97c
doquocviet24 <doquocviet0902@gmail.com>
2021-08-03 13:29:28 +0700

    add login

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -138,4 +138,18 @@ public class UserController {
 		UserCsvExporter exporter = new UserCsvExporter();
 		exporter.export(listUsers, response);
 	}
+	
+	@GetMapping("/users/export/excel")
+	public void exportToExcel(HttpServletResponse response) throws IOException {
+		List<User> listUsers  = service.listAll();
+		UserExcelExporter exporter = new UserExcelExporter();
+		exporter.export(listUsers, response);
+	}
+	
+	@GetMapping("/users/export/pdf")
+	public void exportToPDF(HttpServletResponse response) throws IOException {
+		List<User> listUsers  = service.listAll();
+		UserPDFExporter exporter = new UserPDFExporter();
+		exporter.export(listUsers, response);
+	}
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserCsvExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserCsvExporter.java
@@ -14,16 +14,10 @@ import org.supercsv.prefs.CsvPreference;
 
 import com.shopme.common.entity.User;
 
-public class UserCsvExporter {
+public class UserCsvExporter extends AbstractExporter{
 	
     public void export(List<User> listUsers,HttpServletResponse response) throws IOException {
-    	DateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-    	String timestamp = dateFormater.format(new Date());
-    	String fileName = "users_"+timestamp+".csv";
-    	response.setContentType("text/csv");
-    	String headerKey = "Content-Disposition";
-    	String headerValue = "attacment; filename="+fileName;
-    	response.setHeader(headerKey, headerValue);
+    	super.setResponseHeader(response,"text/csv", "csv");
     	ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
     	String[] csvHearder = {"User ID","E-mail","First Name","LastName","Roles","Enabled"};
     	String[] fieldMapping = {"id","email","firstName","lastName","roles","enabled"};
