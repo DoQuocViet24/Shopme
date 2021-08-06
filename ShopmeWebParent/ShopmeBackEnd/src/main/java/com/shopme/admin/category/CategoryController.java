424e459e3b3fbb69a869e7fdb39fe4ba755bf6b0
doquocviet24 <doquocviet0902@gmail.com>
2021-08-06 18:33:03 +0700

    pagination category

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.FileUploadUtil;
 import com.shopme.admin.user.UserNotFoundException;
+import com.shopme.admin.user.UserService;
 import com.shopme.common.entity.Category;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
@@ -27,12 +28,24 @@ public class CategoryController {
 	private CategoryService service;
 	
 	@GetMapping("/categories")
-	public String listAll(@Param("sortDir") String sortDir, Model model) {
+	public String listFirstPage(@Param("sortDir") String sortDir, Model model) {
+		return listByPage(1,sortDir,model);
+	}
+	
+	@GetMapping("/categories/page/{pageNum}")
+	public String listByPage(@PathVariable(name="pageNum") int pageNum,@Param("sortDir") String sortDir, Model model) {
 		if(sortDir == null || sortDir.isEmpty()) {
 			sortDir = "asc";
 		}
-		List<Category> listCategories = service.listAll(sortDir);
+		CategoryPageInfo pageInfo = new CategoryPageInfo();
+		List<Category> listCategories = service.listByPage(pageInfo,pageNum,sortDir);
 		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+		model.addAttribute("currentPage",pageNum);
+		model.addAttribute("totalPages",pageInfo.getTotalPages());
+		model.addAttribute("totalItems",pageInfo.getTotalElements());
+		model.addAttribute("sortField","name");
+		model.addAttribute("sortDir",sortDir);
 		model.addAttribute("listCategories",listCategories);
 		model.addAttribute("reverseSortDir", reverseSortDir);
 		return "categories/categories";
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -11,6 +11,9 @@ import java.util.TreeSet;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +24,8 @@ import com.shopme.common.entity.Category;
 public class CategoryService {
 	@Autowired
 	private CategoryRepository repo;
-	
-	public List<Category> listAll(String sortDir){
+	private static final int ROOT_CATEGORY_PER_PAGE = 4;
+	public List<Category> listByPage(CategoryPageInfo pageInfo, int pageNum, String sortDir){
 		Sort sort =  Sort.by("name");
 		
 		if(sortDir.equals("desc")) {
@@ -30,8 +33,13 @@ public class CategoryService {
 		}else {
 			sort = sort.ascending();
 		}
-		List<Category> rootCategories = repo.findRootCategories(sort);
+		Pageable pageable = PageRequest.of(pageNum-1,ROOT_CATEGORY_PER_PAGE,sort);
 		
+		Page<Category> pageCategories = repo.findRootCategories(pageable);
+		List<Category> rootCategories  =pageCategories.getContent();
+		
+		pageInfo.setTotalElements(pageCategories.getTotalElements());
+		pageInfo.setTotalPages(pageCategories.getTotalPages());
 		return listHierarchicalCategories(rootCategories,sortDir) ;
 	}
 	
